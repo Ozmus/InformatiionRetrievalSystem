cde47f7bf9521818558a6bcebde72b40f92ea15f
Ozmus <49372146+Ozmus@users.noreply.github.com>
2021-08-06 00:12:08 +0300

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -4,6 +4,10 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+"""
+https://www.scrapingbee.com/blog/crawling-python/
+"""
+
 logging.basicConfig(
     format='%(asctime)s %(levelname)s:%(message)s',
     level=logging.INFO)
@@ -13,14 +17,13 @@ class Crawler:
     def __init__(self, urls=[]):
         self.visited_urls = []
         self.urls_to_visit = urls
-        self.numOfDownloadedURLs = 0;
+        self.numOfDownloadedURLs = 0
         self.webPages_data = [None for _ in range(2501)]
 
     def download_url(self, url):
         try:
             plain_text = requests.get(url).text
             soup = BeautifulSoup(plain_text, "html.parser")
-            appendix = ()
             if( soup.title != None):
                 appendix = ({
                     'url': url,
@@ -42,11 +45,12 @@ class Crawler:
             path = link.get('href')
             if path and path.startswith('/'):
                 path = urljoin(url, path)
+                #print("path: ", path)
             yield path
 
     def add_url_to_visit(self, url):
         if url not in self.visited_urls and url not in self.urls_to_visit and url != None and \
-                url.__contains__("tr") and not url.__contains__(".pdf") and not url.__contains__(".zip") and not url.__contains__(".rar") and not url.__contains__(".wav") and not url.__contains__(".mpg"):
+                (url.__contains__("tr") or url.__contains__("turkce") or url.__contains__("turkey")) and not url.__contains__(".pdf") and not url.__contains__(".zip") and not url.__contains__(".rar") and not url.__contains__(".wav") and not url.__contains__(".mpg"):
             self.urls_to_visit.append(url)
 
     def crawl(self, url):
@@ -56,6 +60,7 @@ class Crawler:
 
     def run(self):
         while self.urls_to_visit and self.numOfDownloadedURLs <= 2500:
+            #print("self.urls_to_visit: ", self.urls_to_visit)
             logging.info(f'numOfDownloadedURLs: {self.numOfDownloadedURLs}')
             url = self.urls_to_visit.pop(0)
             logging.info(f'Crawling: {url}')
@@ -65,7 +70,8 @@ class Crawler:
                 logging.exception(f'Failed to crawl: {url}')
             finally:
                 self.visited_urls.append(url)
-        with open("webPages.json", "r", encoding="utf-8") as file:
+
+        with open("webPages2.json", "r", encoding="utf-8") as file:
             try:
                 temp = json.load(file)
             except:
@@ -75,12 +81,12 @@ class Crawler:
             if(self.webPages_data[i] != None):
                 temp.append(self.webPages_data[i])
 
-        with open("webPages.json", "w", encoding="utf-8") as file:
+        with open("webPages2.json", "w", encoding="utf-8") as file:
             json.dump(temp, file, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
     Crawler(urls=['https://tr.wikipedia.org/wiki/Koronavirüs']).run()
     Crawler(urls=['https://covid19.saglik.gov.tr/']).run()
-    Crawler(urls=['https://www.cnnturk.com/saglik/biontech-asisindan-sonra-agri-kesici-icilir-mi-biontech-asi-olduktan-sonra-antibiyotik-alinir-mi']).run()
-    Crawler(urls=['https://www.unicef.org/turkey/gerçek-mi-efsane-mi-koronavirüs-covid-19-hakkında-ne-kadar-bilgi-sahibisiniz']).run()
+    Crawler(urls=['https://www.bbc.com/turkce/topics/ck0r47pk362t']).run()
+    Crawler(urls=['https://www.acibadem.com.tr/koronavirus/koronavirus-corona-virusu-nedir-belirtileri/']).run()
