6554d69c127e7715b8f72843702e0c533f943f88
Ozmus <49372146+Ozmus@users.noreply.github.com>
2021-08-18 22:44:53 +0300

    Update pageRetrieval.py

--- pageRetrieval.py
+++ pageRetrieval.py
@@ -11,8 +11,6 @@ with open("webPages.json", "r", encoding="utf-8") as file:
 with open("invertedList.json", "r", encoding="utf-8") as file:
     invertedList = json.load(file)
 
-query = "Koronavirüs güncel vaka sayısı nedir?"
-tokenizedQuery = []
 # JVM başlat
 # Aşağıdaki adresleri java sürümünüze ve jar dosyasının bulunduğu klasöre göre değiştirin
 jpype.startJVM("C:\\Program Files\\Java\\jdk-11.0.1\\bin\\server\\jvm.dll",
@@ -26,77 +24,100 @@ Zemberek = jpype.JClass("net.zemberek.erisim.Zemberek")
 # zemberek nesnesini oluştur
 zemberek = Zemberek(tr)
 
-#Noktalama isaretlerinin atilmasi
-nok = '''!‡()|-–—[]{};:'"\, ’<>./?@#$%^&*_~'''
+def findWebPages(query):
+    #Remove punctuations
+    nok = '''!‡()|-–—[]{};:'"\, ’<>./?@#$%^&*_~'''
+    for char in query:
+        if char in nok:
+            query.replace(char, " ")
 
+    #Tokenize Query
+    tokenizedQuery = []
+    for word in query.split():
+        if word.strip() > '':
+            yanit = zemberek.kelimeCozumle(word)
+            if yanit:
+                word = "{}".format(yanit[0]).split("Kok: ")[1].split()[0]
+            tokenizedQuery.append(word.lower())
 
-for char in query:
-    if char in nok:
-        query.replace(char, " ")
-
-for word in query.split():
-    if word.strip() > '':
-        yanit = zemberek.kelimeCozumle(word)
-        if yanit:
-            word = "{}".format(yanit[0]).split("Kok: ")[1].split()[0]
-        tokenizedQuery.append(word.lower())
-
-
-idft = {}
-for word in tokenizedQuery:
-    try:
-        idft[word] = math.log10(len(webPages) / len(invertedList[word]))
-    except:
-        continue
-
-scores = {}
-for word in tokenizedQuery:
-    try:
-        for doc in invertedList[word].keys():
-            try:
-                scores[doc] += (1 + math.log10(invertedList[word][doc])) * idft[word]
-            except:
-                scores[doc] = (1 + math.log10(invertedList[word][doc])) * idft[word]
-    except:
-        continue
-
-sortedScores = sorted(scores.items(), key=lambda x:x[1], reverse=True)
-for docID in sortedScores:
-    print(webPages[int(docID[0])]["url"])
-
-# BM25
-def totalTermsInDoc(docId):
-    response = 0
-    numOfTerms = 0
-    for word in invertedList:
+    #Calculate idf scores of query words
+    idft = {}
+    for word in tokenizedQuery:
         try:
-            numOfTerms += 1
-            response += invertedList[word][docId]
+            idft[word] = math.log10(len(webPages) / len(invertedList[word]))
         except:
             continue
-    return (response, (response / numOfTerms))
+
+    #Calculate page scores
+    scores = {}
+    for word in tokenizedQuery:
+        try:
+            for doc in invertedList[word].keys():
+                try:
+                    scores[doc] += (1 + math.log10(invertedList[word][doc])) * idft[word]
+                except:
+                    scores[doc] = (1 + math.log10(invertedList[word][doc])) * idft[word]
+        except:
+            continue
+
+    #Sort Pages
+    sortedScores = sorted(scores.items(), key=lambda x:x[1], reverse=True)
+    """
+       # BM25
+       def totalTermsInDoc(docId):
+           response = 0
+           numOfTerms = 0
+           for word in invertedList:
+               try:
+                   numOfTerms += 1
+                   response += invertedList[word][docId]
+               except:
+                   continue
+           return (response, (response / numOfTerms))
 
 
-BM25Scores = {}
-for word in tokenizedQuery:
+       BM25Scores = {}
+       for word in tokenizedQuery:
+           try:
+               for doc in invertedList[word].keys():
+                   try:
+                       L = totalTermsInDoc(doc)
+                       BM25Scores[doc] += (invertedList[word][doc] / (1.5 * L[0] / L[1] + invertedList[word][doc] + 0.5 )) * math.log10( (len(webPages) - len(invertedList[word]) + 0.5) / (len(invertedList[word]) + 0.5) )
+                   except:
+                       BM25Scores[doc] = (invertedList[word][doc] / (1.5 * L[0] / L[1] + invertedList[word][doc] + 0.5 )) * math.log10( (len(webPages) - len(invertedList[word]) + 0.5) / (len(invertedList[word]) + 0.5) )
+
+           except:
+               continue
+
+       print("BM25 Scores:")
+       print(BM25Scores)
+       sortedBM25 = sorted(BM25Scores.items(), key=lambda x:x[1], reverse=True)
+
+       with open("BM25.json", "w", encoding="utf-8") as file:
+           json.dump(sortedBM25, file, ensure_ascii=False, indent=4)
+
+       """
+
+    return sortedScores
+
+while True:
+    choice = input("1. Sorgu girme\n2. Çıkış\n Seçiminiz: ")
     try:
-        for doc in invertedList[word].keys():
-            try:
-                L = totalTermsInDoc(doc)
-                BM25Scores[doc] += (invertedList[word][doc] / (1.5 * L[0] / L[1] + invertedList[word][doc] + 0.5 )) * math.log10( (len(webPages) - len(invertedList[word]) + 0.5) / (len(invertedList[word]) + 0.5) )
-            except:
-                BM25Scores[doc] = (invertedList[word][doc] / (1.5 * L[0] / L[1] + invertedList[word][doc] + 0.5 )) * math.log10( (len(webPages) - len(invertedList[word]) + 0.5) / (len(invertedList[word]) + 0.5) )
-
+        choiceInt = int(choice)
+        if(choiceInt == 1):
+            query = input("Bir sorgu giriniz: ")
+            sortedScores = findWebPages(query)
+            if(len(sortedScores) <= 0):
+                sortedScores = findWebPages("Koronavirüs")
+            for docID in sortedScores:
+                print(webPages[int(docID[0])]["url"])
+        elif(choiceInt == 2):
+            print("İyi günler dileriz.")
+            break;
+        else:
+            print("Geçersiz seçenek. 1 veya 2 yi seçiniz.")
     except:
-        continue
+        print("Geçersiz seçenek. 1 veya 2 yi seçiniz.")
 
-print("BM25 Scores:")
-print(BM25Scores)
-sortedBM25 = sorted(BM25Scores.items(), key=lambda x:x[1], reverse=True)
-
-with open("BM25.json", "w", encoding="utf-8") as file:
-    json.dump(sortedBM25, file, ensure_ascii=False, indent=4)
-
-
-#JVM kapat
+# JVM kapat
 jpype.shutdownJVM()
